e93463c9058b9274b67b758e12fbfa45e90ae4aa
Loukkka <louka.poulbriere@etu.unilim.fr>
2025-03-12 15:26:50 +0100

    exo1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://Loukkka.github.io',
-  base: 'TestTd',
-})
+  site: "https://Loukkka.github.io",
+  base: "TestTd",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
