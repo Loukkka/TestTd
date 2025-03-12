bcde319089464c915dcffe1b62c02f50454c988f
Loukkka <louka.poulbriere@etu.unilim.fr>
2025-03-12 13:41:08 +0100

    TD1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Loukkka.gitlab.io',
+  base: 'TestTd',
+  outDir: 'public',
+  publicDir: 'static',
+});
